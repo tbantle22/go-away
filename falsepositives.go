35210db67e4ca73ffdc1823511cc766c5d20d25c
Taylor Bantle <taylor@dolthub.com>
2022-06-23 16:16:18 -0700

    Add more false positives

--- falsepositives.go
+++ falsepositives.go
@@ -2,12 +2,14 @@ package goaway
 
 // DefaultFalsePositives is a list of words that may wrongly trigger the DefaultProfanities
 var DefaultFalsePositives = []string{
+	"analy", // analysis, analytics
 	"arsenal",
 	"assassin",
 	"assaying", // was saying
 	"assert",
 	"assign",
 	"assimil",
+	"assist",
 	"associat",
 	"assum", // assuming, assumption, assumed
 	"assur", // assurance
@@ -38,8 +40,6 @@ var DefaultFalsePositives = []string{
 	"horniman",
 	"hotwater",
 	"identit",
-	"kassa", // kassandra
-	"kassi", // kassie, kassidy
 	"lass", // class
 	"leafage",
 	"libshitz",
